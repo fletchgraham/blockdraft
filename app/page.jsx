c323415644b8d8a49c88b7559d8f1022bab9c210
fletchgraham <sfletchgraham@gmail.com>
2024-11-06 11:45:46 -0700

    better message

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,16 @@ import { getUserFromCookies } from "../lib/getUser";
 export default async function Page() {
   const user = await getUserFromCookies();
   if (user) {
-    return <div>Welcome {user.username}</div>;
+    return (
+      <>
+        <h1 className="text-center text-2xl font-bold mb-5">Coming Soon!</h1>
+        <p className="text-center">
+          BlockDraft AI is a platform that allows you to create curated content
+          with an AI-enabled block editor. It's currently in development, so
+          come back soon for functionality!
+        </p>
+      </>
+    );
   }
   return (
     <>
