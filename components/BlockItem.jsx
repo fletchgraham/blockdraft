2e33b7d522047477c2c6032a8142e2e03d05aec6
fletchgraham <sfletchgraham@gmail.com>
2024-11-08 08:30:06 -0700

    add wip menu to each block

--- components/BlockItem.jsx
+++ components/BlockItem.jsx
@@ -21,6 +21,17 @@ export default function BlockItem({ block }) {
           {block.title}
         </a>
         <p className="text-sm text-gray-500">{block.text}</p>
+        <details className="dropdown">
+          <summary className="btn m-1">open or close</summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Item 2</a>
+            </li>
+          </ul>
+        </details>
       </div>
     </div>
   );
