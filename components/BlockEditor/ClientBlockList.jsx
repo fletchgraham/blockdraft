28a00e2222f6ccd59a91175a507459871e7f425e
fletchgraham <sfletchgraham@gmail.com>
2024-11-14 17:11:02 -0700

    add a placeholder modal for new custom block

--- components/BlockEditor/ClientBlockList.jsx
+++ components/BlockEditor/ClientBlockList.jsx
@@ -5,6 +5,7 @@ import { Droppable } from "@hello-pangea/dnd";
 import ClientBlockItem from "./ClientBlockItem";
 
 export default function ClientBlockList({ blocks, title, droppableId }) {
+  const newBlockModalId = `newBlockModal-${droppableId}`;
   return (
     <Droppable droppableId={droppableId}>
       {(provided) => (
@@ -19,7 +20,32 @@ export default function ClientBlockList({ blocks, title, droppableId }) {
               <ClientBlockItem key={block._id} block={block} index={index} />
             ))}
             {provided.placeholder}
+            <li>
+              <button
+                className="btn btn-ghost shadow w-full"
+                onClick={() =>
+                  document.getElementById(newBlockModalId).showModal()
+                }
+              >
+                + New Block
+              </button>
+            </li>
           </ul>
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <dialog id={newBlockModalId} className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
       )}
     </Droppable>
