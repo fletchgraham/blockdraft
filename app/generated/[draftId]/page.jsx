c01fb33ae8356d4bbb202a2142f15a2f6717b42b
fletchgraham <sfletchgraham@gmail.com>
2024-11-15 12:17:40 -0700

    fake progress bar on generate page

--- app/generated/[draftId]/page.jsx
+++ app/generated/[draftId]/page.jsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { getUserFromCookies } from "@/lib/getUser";
 import { getBlocksForDraft } from "@/lib/blocks";
+import GenerateProgress from "@/components/GenerateProgress";
 
 export default async function GeneratedPage({ params }) {
   const user = await getUserFromCookies();
@@ -11,12 +12,20 @@ export default async function GeneratedPage({ params }) {
   const draftId = await params.draftId;
   const blocks = await getBlocksForDraft(params.draftId);
 
+  // get blocks with url prop
+  const blocksWithUrl = blocks.filter((block) => block.url);
+  const blocksWithUrlandSummary = blocksWithUrl.map(
+    async (block) => block.summary
+  );
+
   return (
     <div>
+      <GenerateProgress initialComplete={0} initialTotal={blocks.length} />
       <h1>Generated Page for {draftId}</h1>
+      <p>{`URL Blocks Summarized: ${blocksWithUrlandSummary.length} / ${blocksWithUrl.length}`}</p>
       <ul>
         {blocks.map((block) => (
-          <li key={block._id.toString()}>{block.title}</li>
+          <li key={block._id.toString()}>{block.title || block.content}</li>
         ))}
       </ul>
     </div>
---
+++ components/GenerateProgress.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function GenerateProgress({ initialComplete, initialTotal }) {
+  const [complete, setComplete] = useState(initialComplete);
+
+  console.log(complete, initialTotal);
+
+  // make a little timeout to simulate progress updating
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (complete >= initialTotal) {
+        clearInterval(interval);
+        return;
+      }
+      setComplete((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Generating Your Article</h1>
+      <p>Your article is being generated.</p>
+      <progress
+        className="progress w-full"
+        value={complete}
+        max={initialTotal}
+      />
+    </div>
+  );
+}
