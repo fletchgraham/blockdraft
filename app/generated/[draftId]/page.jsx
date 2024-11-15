c11984646773b25ec1a2c33c764df7aebc05efc4
fletchgraham <sfletchgraham@gmail.com>
2024-11-15 11:47:21 -0700

    stub for generated page

---
+++ app/generated/[draftId]/page.jsx
@@ -0,0 +1,8 @@
+export default function GeneratedPage({ params }) {
+  return (
+    <div>
+      <h1>Generated Page for {params.draftId}</h1>
+      <p>This is a generated page.</p>
+    </div>
+  );
+}
