04a72005e87d7dab674df74eda882e377c7d34cc
fletchgraham <sfletchgraham@gmail.com>
2024-11-17 13:06:59 -0700

    add placeholder modals to the draft cards for delete

--- components/DraftCard.jsx
+++ components/DraftCard.jsx
@@ -28,9 +28,27 @@ export default function DraftCard({ draft }) {
         </Link>
         <p>{draft.blocks?.length || 0} blocks</p>
         <div className="card-actions justify-end">
-          {/* <Link href={`/drafts/${draft._id}`}>
-            <button className="btn btn-primary">Open Draft</button>
-          </Link> */}
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <button
+            className="btn"
+            onClick={() => document.getElementById("my_modal_1").showModal()}
+          >
+            Delete Draft
+          </button>
+          <dialog id="my_modal_1" className="modal">
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Cancel</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
         </div>
       </div>
     </div>
