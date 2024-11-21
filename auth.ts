969afa2900cfc8edfee19b45855ea8b96867cd15
fletchgraham <sfletchgraham@gmail.com>
2024-11-21 14:09:43 -0700

    add mongo db adapter for auth.
    and fix the issue with the adapter trying to run in the edge runtime. something about middleware. here's more info https://authjs.dev/guides/edge-compatibility

--- auth.ts
+++ auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
+import authConfig from "./auth.config";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import client from "./lib/mongodb";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  adapter: MongoDBAdapter(client),
+  session: { strategy: "jwt" },
+  ...authConfig,
 });
