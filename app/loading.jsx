9ef132ce47c5c4732c1c6729520f2a711a236708
fletchgraham <sfletchgraham@gmail.com>
2024-11-21 07:30:52 -0700

    center loading vertically

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="text-center">
+    <div className="flex justify-center items-center h-screen">
       <p>
         <span className="loading loading-bars loading-lg"></span>
       </p>
