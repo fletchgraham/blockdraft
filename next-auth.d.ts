9c68da4b3b501586fc15862ed130a784b8d97f02
fletchgraham <sfletchgraham@gmail.com>
2024-11-22 09:46:48 -0700

    extend some types. i think.

---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      userId: string;
+    } & DefaultSession["user"];
+  }
+
+  interface JWT {
+    userId: string;
+  }
+}
