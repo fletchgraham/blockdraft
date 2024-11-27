8412ed5796f90406435a54bbf0d4c223247a8aa0
fletchgraham <sfletchgraham@gmail.com>
2024-11-27 14:22:01 -0700

    adjust topbar copy

--- components/Landing/Topbar.jsx
+++ components/Landing/Topbar.jsx
@@ -19,9 +19,9 @@ export const Topbar = () => {
     <>
       <div className="bg-neutral py-1.5 text-center text-xs text-neutral-content md:text-sm">
         <span>
-          Revamp Your Control: Code
-          <span className="mx-1 font-semibold text-warning">DAISYUI</span>
-          for a 20% SaaS discount 🚀
+          Streamline Your Content Workflow: Code
+          <span className="mx-1 font-semibold text-warning">BLOCKDRAFT</span>
+          for a 20% discount 🚀
         </span>
       </div>
 
@@ -42,7 +42,7 @@ export const Topbar = () => {
                     <Menu className="min-h-full w-80 gap-2 bg-base-100 p-4 text-base-content">
                       <Menu.Item className="font-medium">
                         <a href="index.html" className="text-xl font-bold">
-                          SaaS Landing
+                          BlockDraft AI
                         </a>
                       </Menu.Item>
 
@@ -75,7 +75,7 @@ export const Topbar = () => {
               </div>
 
               <a href="#" className="text-xl font-bold tracking-tighter">
-                SaaS Landing
+                BlockDraft AI
               </a>
             </Navbar.Start>
 
