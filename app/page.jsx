3420cde05868dc0a0819a9890f19535f0e37c8ab
fletchgraham <sfletchgraham@gmail.com>
2025-05-05 16:09:09 -0600

    if logged in go straight to editor

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
+import { auth } from "@/auth";
 import Landing from "@/components/Landing/Landing";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (session) {
+    redirect("/edit");
+  }
   return <Landing />;
 }
